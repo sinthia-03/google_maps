8c60f3d39a8bbe637ccbbba2515905a5f0d413ae
sinthia-03 <sinthiaorita37@gmail.com>
2026-05-17 23:56:24 +0600

    .

---
+++ lib/google_maps_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GoogleMapsScreen extends StatefulWidget {
+  const GoogleMapsScreen({super.key});
+
+  @override
+  State<GoogleMapsScreen> createState() => _GoogleMapsScreenState();
+}
+
+class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
