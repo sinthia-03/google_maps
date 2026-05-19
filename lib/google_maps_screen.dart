75c5d933630e4eee6e2fdb060a0c0a0c5a429c4b
sinthia-03 <sinthiaorita37@gmail.com>
2026-05-19 21:13:31 +0600

    map shapes

--- lib/google_maps_screen.dart
+++ lib/google_maps_screen.dart
@@ -42,7 +42,100 @@ class _GoogleMapsScreenState extends State<GoogleMapsScreen> {
         onCameraMoveStarted: (){
           print('Hide/do something');
         },
-        mapType: MapType.hybrid,
+        mapType: MapType.normal, //map type set
+        markers: <Marker>{ // marker set
+          Marker(
+              markerId: MarkerId('home'),
+            position: LatLng(23.8812505449456, 90.3209863230586),
+            infoWindow: InfoWindow(
+              title: 'Home',
+              onTap: (){
+
+              }
+            )
+          ),
+          Marker(
+              markerId: MarkerId('office'),
+              position: LatLng(23.87710623884813, 90.32010555267334),
+              infoWindow: InfoWindow(
+                  title: 'office',
+                onTap: (){
+
+                }
+              ),
+            // marker er color set
+           icon: BitmapDescriptor.defaultMarkerWithHue(
+               BitmapDescriptor.hueAzure),
+            //custom icon set
+            //icon: await BitmapDescriptor.asset(
+              //  ImageConfiguration(), 'path of icon');
+
+
+          ),
+        },
+        circles: <Circle>{
+          Circle(
+              circleId: CircleId('red - Zoon'),
+            center: LatLng(23.885769643281883, 90.32407857477665),
+            radius: 200,
+            fillColor: Colors.red.withAlpha(50),
+            strokeColor: Colors.red,
+            strokeWidth: 3,
+            consumeTapEvents: true,
+            onTap: (){
+            print('tapped on circle');
+            }
+            
+          ),
+          Circle(
+              circleId: CircleId('green-Zoon'),
+              center: LatLng(23.87306082909841, 90.30978742986917),
+              radius: 200,
+              fillColor: Colors.green.withAlpha(50),
+              strokeColor: Colors.green,
+              strokeWidth: 3,
+              consumeTapEvents: true,
+              onTap: (){
+                print('tapped on  green zoon circle');
+              }
+
+          )
+        },
+        polylines:<Polyline>{
+          Polyline(
+            polylineId: PolylineId('home-to-office'),
+            points: [
+
+              LatLng(23.8812505449456, 90.3209863230586),
+              LatLng(23.87710623884813, 90.32010555267334),
+              LatLng(23.87306082909841, 90.30978742986917),
+            ],
+            width: 4,
+            color: Colors.blue,
+            endCap: .roundCap,
+            visible: true,
+            startCap: .roundCap
+          )
+        },
+        polygons: <Polygon>{
+          Polygon(
+            polygonId: PolygonId('danger-area'),
+            points: [
+              LatLng(23.868675578647682, 90.32634034752846),
+              LatLng(23.866857724160475, 90.33268008381128),
+              LatLng(23.868910435368935, 90.3347135335207)
+            ],
+            fillColor: Colors.red.withAlpha(50),
+            strokeColor: Colors.orange,
+            strokeWidth: 3,
+            consumeTapEvents: true,
+            onTap: (){
+              print('Tapped on danger area');
+            }
+          )
+        },
+
+
       ),
     );
   }
